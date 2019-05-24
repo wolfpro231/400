390da526998d58818ca678dd60d024b432eb1f0d
wolfpro231 <48035876+wolfpro231@users.noreply.github.com>
2019-05-24 22:57:59 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,22 +11,22 @@ client.on('message', message => {
       
   if (message.content.startsWith(adminprefix + 'bt2')) {
     client.user.setGame(argresult);
-      message.channel.send(`**✅   ${argresult}**`)
+      message.channel.send(`**تـــ تغيير الحالة ــم   ${argresult}**`)
   } else 
      if (message.content === (adminprefix + "bt3")) {
     message.guild.leave();        
   } else  
   if (message.content.startsWith(adminprefix + 'bt5')) {
   client.user.setActivity(argresult, {type:'WATCHING'});
-      message.channel.send(`**✅   ${argresult}**`)
+      message.channel.send(`**تـــ تغيير الحالة ــم   ${argresult}**`)
   } else 
   if (message.content.startsWith(adminprefix + 'bt4')) {
   client.user.setActivity(argresult , {type:'LISTENING'});
-      message.channel.send(`**✅   ${argresult}**`)
+      message.channel.send(`**تـــ تغيير الحالة ــم   ${argresult}**`)
   } else 
   if (message.content.startsWith(adminprefix + 'bt')) {
     client.user.setGame(argresult, "https://www.twitch.tv/dream");
-      message.channel.send(`**✅**`)
+      message.channel.send(`**تـــ تغيير الحالة ــم**`)
   }
   if (message.content.startsWith(adminprefix + 'setname')) {
   client.user.setUsername(argresult).then
